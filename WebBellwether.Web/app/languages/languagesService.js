7a70dd6227396846a897d3f138528b17d5b27da0
Bartosz Ciurla <itsgoodtoride@gmail.com>
2015-10-30 01:04:27 +0100

    management languages , this is not easy

--- WebBellwether.Web/app/languages/languagesService.js
+++ WebBellwether.Web/app/languages/languagesService.js
@@ -3,14 +3,14 @@
         .module('webBellwether')
         .factory('languagesService', ['$http','$resource', 'ngAuthSettings', function ($http,$resource, ngAuthSettings) {
             var serviceBase = ngAuthSettings.apiServiceBaseUri;
-            var getContent = function (lang) {
+            var getLanguageContent = function (lang) {
                 var languageFilePath = '/appData/translations/translation_' + lang + '.json';
                 return $resource(languageFilePath).get(function (data) {
                     return data;
                 });
             };
             var managementLanguagesServiceFactory = {};
-            managementLanguagesServiceFactory.GetContent = getContent;
+            managementLanguagesServiceFactory.GetLanguageContent = getLanguageContent;
             return managementLanguagesServiceFactory;
 
         }]);
--- WebBellwether.Web/app/languages/managementLanguagesController.js
+++ WebBellwether.Web/app/languages/managementLanguagesController.js
@@ -2,17 +2,27 @@
     angular
         .module('webBellwether')
         .controller('managementLanguagesController', ['$scope', '$timeout', 'languagesService', 'translateService', 'sharedService', function ($scope, $timeout, languagesService, translateService, sharedService) {
-
-            //only test 
-            $scope.languageForEdit = '';
-            $scope.myContent = '';
-            $scope.getContent = function (lang) {
-                
-                $scope.myContent = languagesService.GetContent(lang);
-               
+            $scope.currentPage = 0;
+            $scope.pageSize = 20;
+            $scope.numberOfPages = function () {
+                if (Object.keys($scope.selectedLanguageContent).length > 0)
+                    return Math.ceil(Object.keys($scope.selectedLanguageContent).length / $scope.pageSize);
+                else
+                    return 0;
+            };
+            $scope.quotientKeyPageSize = function () {
+                return (Object.keys($scope.selectedLanguageContent).length / $scope.pageSize - 1);
+            }
+            $scope.goToTop = function () {
+                window.scrollTo(0, 470);
             };
 
-            $scope.getContent("2");
+            $scope.languageForEdit = '';
 
+            $scope.selectedLanguageContent = '';
+            $scope.getLanguageContent = function (lang) {                
+                $scope.selectedLanguageContent = languagesService.GetLanguageContent(lang);               
+            };
+      
         }]);
 })();
