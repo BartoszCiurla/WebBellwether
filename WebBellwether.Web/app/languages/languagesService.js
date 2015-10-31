bac10a65471e83119a2e1c967d3bcb2734e5bccb
Bartosz Ciurla <itsgoodtoride@gmail.com>
2015-10-31 01:16:56 +0100

    pagination in management languages works , but i have several little problem to resorve

--- WebBellwether.Web/app/languages/languagesService.js
+++ WebBellwether.Web/app/languages/languagesService.js
@@ -1,14 +1,32 @@
 ﻿(function () {
     angular
         .module('webBellwether')
-        .factory('languagesService', ['$http','$resource', 'ngAuthSettings', function ($http,$resource, ngAuthSettings) {
+        .factory('languagesService', ['$http', '$resource', 'ngAuthSettings', function ($http, $resource, ngAuthSettings) {
             var serviceBase = ngAuthSettings.apiServiceBaseUri;
+
             var getLanguageContent = function (lang) {
                 var languageFilePath = '/appData/translations/translation_' + lang + '.json';
-                return $resource(languageFilePath).get(function (data) {
+                var myResource = $resource(languageFilePath).get();
+                return myResource.$promise.then(function (data) {
                     return data;
                 });
             };
+            //var result = [];
+            //for (key in data) {
+            //    result.push({ Key: key, Value: data[key] });
+            //}
+            //return result;
+
+            //var resultData = [];
+            //var result = $resource(languageFilePath);
+            //result.get()
+            //.$promise.then(function (x) {
+            //    for (key in x) {
+            //        resultData.push({ Key: key, Value: data[key] });
+            //    }
+            //    return resultData;
+            //})
+
             var managementLanguagesServiceFactory = {};
             managementLanguagesServiceFactory.GetLanguageContent = getLanguageContent;
             return managementLanguagesServiceFactory;
--- WebBellwether.Web/app/languages/managementLanguagesController.js
+++ WebBellwether.Web/app/languages/managementLanguagesController.js
@@ -4,14 +4,17 @@
         .controller('managementLanguagesController', ['$scope', '$timeout', 'languagesService', 'translateService', 'sharedService', function ($scope, $timeout, languagesService, translateService, sharedService) {
             $scope.currentPage = 0;
             $scope.pageSize = 20;
+            //$scope.numberOfPages = function () {
+            //    if (Object.keys($scope.selectedLanguageContent).length > 0)
+            //        return Math.ceil(Object.keys($scope.selectedLanguageContent).length / $scope.pageSize);
+            //    else
+            //        return 0;
+            //};
+            //$scope.quotientKeyPageSize = function () {
+            //    return (Object.keys($scope.selectedLanguageContent).length / $scope.pageSize - 1);
+            //}
             $scope.numberOfPages = function () {
-                if (Object.keys($scope.selectedLanguageContent).length > 0)
-                    return Math.ceil(Object.keys($scope.selectedLanguageContent).length / $scope.pageSize);
-                else
-                    return 0;
-            };
-            $scope.quotientKeyPageSize = function () {
-                return (Object.keys($scope.selectedLanguageContent).length / $scope.pageSize - 1);
+                return Math.ceil($scope.selectedLanguageContent.length / $scope.pageSize);
             }
             $scope.goToTop = function () {
                 window.scrollTo(0, 470);
@@ -19,10 +22,16 @@
 
             $scope.languageForEdit = '';
 
-            $scope.selectedLanguageContent = '';
-            $scope.getLanguageContent = function (lang) {                
-                $scope.selectedLanguageContent = languagesService.GetLanguageContent(lang);               
-            };
-      
+            $scope.selectedLanguageContent = [];
+            $scope.getLanguageContent = function (lang) {
+                //$scope.selectedLanguageContent = languagesService.GetLanguageContent(lang);
+                $scope.selectedLanguageContent = [];
+                languagesService.GetLanguageContent(lang).then(function (x) {
+                    for (key in x) {
+                        if(key.indexOf(1) != '$' && key != "toJSON")
+                            $scope.selectedLanguageContent.push({ key: key, value: x[key] });
+                    }
+                })                
+            };      
         }]);
 })();
