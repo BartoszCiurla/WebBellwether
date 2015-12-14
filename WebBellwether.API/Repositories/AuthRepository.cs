b340fc79738e4f7c15ab1b20ac5e77025f442cab
Bartosz Ciurla <itsgoodtoride@gmail.com>
2015-12-15 00:34:06 +0100

    pagination improved (visible only when the number of pages larger than 1) . New problem is an responsiveness in search form (only ie) f...

--- WebBellwether.API/Repositories/AuthRepository.cs
+++ WebBellwether.API/Repositories/AuthRepository.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using WebBellwether.API.Context;
 using WebBellwether.API.Entities;
-using WebBellwether.API.Models;
 using WebBellwether.API.Models.Auth;
 
 namespace WebBellwether.API.Repositories
